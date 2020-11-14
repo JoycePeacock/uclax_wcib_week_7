0e450961abde000486c644e07d0c06c8a2909727
Joyce <jpeacock@cypresscollege.edu>
2020-11-13 16:47:04 -0800

    ecard

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 jQuery(function() {
 
-	$('#my_button').click(function () {
-	});
+	let my_switch = false;
 
+	$('#my_button').click(function () {
+		
+		if ( my_switch == false ) {
+			my_switch = true;
+
+		$(this).html('Click for traditional baby').removeClass('btn-primary').addClass('btn-gothic');
+		$('body').css({'background-color': 'rgb(45, 5, 0)', 'background-image': 'url("../images/bats.png")'});
+		$('#my_image').attr('src', 'images/storkgoth.png');
+	} else {
+			my_switch = false;
+
+		$(this).html('Click for goth baby').removeClass('btn-gothic').addClass('btn-primary');
+		$('body').css({'background-color': 'rgb(187, 251, 211)', 'background-image': 'url("../images/clouds.png")'});
+		$('#my_image').attr('src', 'images/stork2.png');
+	}
+
+	//
+	// 	if ( $('.square.alt').hasClass('red') ) {
+	// 	$('.square.alt').removeClass('red');
+	// } else {
+	// 	$('.square.alt').addClass('red');
+	// }
+
+	});
 });
