b630dea6617f77661321b59b034c255d1d4e737e
Joyce <jpeacock@cypresscollege.edu>
2020-11-13 17:05:40 -0800

    jquery change

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,13 @@ jQuery(function() {
 	let my_switch = false;
 
 	$('#my_button').click(function () {
-		
+
 		if ( my_switch == false ) {
 			my_switch = true;
 
+		var imageUrl = 'images/bats/png'
 		$(this).html('Click for traditional baby').removeClass('btn-primary').addClass('btn-gothic');
-		$('body').css({'background-color': 'rgb(45, 5, 0)', 'background-image': 'url("../images/bats.png")'});
+		$('body').css({'background-color': 'rgb(45, 5, 0)', 'background-image': 'url("+  urlImage + ")'});
 		$('#my_image').attr('src', 'images/storkgoth.png');
 	} else {
 			my_switch = false;
