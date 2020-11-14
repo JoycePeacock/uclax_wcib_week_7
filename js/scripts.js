10f36825b073a297871c6d448600f0e6b8c33ffc
Joyce <jpeacock@cypresscollege.edu>
2020-11-13 17:10:55 -0800

    changing images

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,8 @@ jQuery(function() {
 		if ( my_switch == false ) {
 			my_switch = true;
 
-		var imageUrl = '../images/bats/png'
+		var imageUrl = 'images/bats.png'
+		var imageUrl2 - "images/clouds.png"
 		$(this).html('Click for traditional baby').removeClass('btn-primary').addClass('btn-gothic');
 		$('body').css({'background-color': 'rgb(45, 5, 0)', 'background-image': 'url("+  imageUrl + ")'});
 		$('#my_image').attr('src', 'images/storkgoth.png');
@@ -15,7 +16,7 @@ jQuery(function() {
 			my_switch = false;
 
 		$(this).html('Click for goth baby').removeClass('btn-gothic').addClass('btn-primary');
-		$('body').css({'background-color': 'rgb(187, 251, 211)', 'background-image': 'url("../images/clouds.png")'});
+		$('body').css({'background-color': 'rgb(187, 251, 211)', 'background-image': 'url("+  imageUrl2 + ")'});
 		$('#my_image').attr('src', 'images/stork2.png');
 	}
 
