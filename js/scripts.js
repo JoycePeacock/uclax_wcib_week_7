f2ca7b2d8e3d89b80d543bde0307e258762f5ffe
Joyce <jpeacock@cypresscollege.edu>
2020-11-13 22:56:19 -0800

    take away

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,14 @@ jQuery(function() {
 		if ( my_switch == false ) {
 			my_switch = true;
 
-		$(this).html('Click for traditional baby').removeClass('btn-primary').addClass('btn-gothic');
-		$('body').css('background-color': 'rgb(45, 5, 0)', 'background-image': 'url('../images/bats.png')';
+		$(this).html('I'm traditional, baby!').removeClass('btn-primary').addClass('btn-gothic');
+		$('body').css('background-color': 'rgb(45, 5, 0)')';
 		$('#my_image').attr('src', 'images/storkgoth.png');
 	} else {
 			my_switch = false;
 
-		$(this).html('Click for goth baby').removeClass('btn-gothic').addClass('btn-primary');
-		$('body').css('background-color': 'rgb(187, 251, 211)', 'background-image': 'url('../images/clouds.png')';
+		$(this).html('I'm goth, baby!').removeClass('btn-gothic').addClass('btn-primary');
+		$('body').css('background-color': 'rgb(187, 251, 211)')';
 		$('#my_image').attr('src', 'images/stork2.png');
 	}
 
