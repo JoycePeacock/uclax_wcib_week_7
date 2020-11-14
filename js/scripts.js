5395fa21dc30792ab85e51b7e766f91e39345c54
Joyce <jpeacock@cypresscollege.edu>
2020-11-13 22:59:44 -0800

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,13 @@ jQuery(function() {
 		if ( my_switch == false ) {
 			my_switch = true;
 
-		$(this).html('I'm traditional, baby!').removeClass('btn-primary').addClass('btn-gothic');
+		$(this).html('I\'m traditional, baby!').removeClass('btn-primary').addClass('btn-gothic');
 		$('body').css('background-color': 'rgb(45, 5, 0)')';
 		$('#my_image').attr('src', 'images/storkgoth.png');
 	} else {
 			my_switch = false;
 
-		$(this).html('I'm goth, baby!').removeClass('btn-gothic').addClass('btn-primary');
+		$(this).html('I\'m goth, baby!').removeClass('btn-gothic').addClass('btn-primary');
 		$('body').css('background-color': 'rgb(187, 251, 211)')';
 		$('#my_image').attr('src', 'images/stork2.png');
 	}
